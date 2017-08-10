c99b14546e7157bbd0071e2b78c70e6179287fda
mthorry <mthorry@gmail.com>
2017-08-10 18:07:37 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,7 @@
 # Add seed data here. Seed your database with `rake db:seed`
 sophie = Owner.create(name: "Sophie")
+sam = Owner.create(name: "Sam")
 Pet.create(name: "Maddy", owner: sophie)
 Pet.create(name: "Nona", owner: sophie)
+Pet.create(name: "Stanley", owner: sam)
+Pet.create(name: "Kevin", owner: sam)
